dfae21ec0f5cc7e58c0204fd12646818cddeb64a
TKSkaushalsaraswat <thekaushalsaraswat@gmail.com>
2020-06-12 01:13:43 +0630

    add

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,6 +1,5 @@
 import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
-import Users from '../pages/Users';
 
 const Sidebar = () => {
   return (
@@ -17,21 +16,41 @@ const Sidebar = () => {
             <h4>Kaushal</h4>
           </Link>
         </center>
-        <Link
-          className="link"
-          data-toggle="modal"
-          data-target="#myModal"
-          to="#"
-        >
+        <Link class="link" data-toggle="dropdown">
           <i className="fas fa-user-circle"></i>
           <span>Users</span>
         </Link>
+        <div class="dropdown-menu customClassForDropDown">
+          <Link to="/" className="users">
+            <img src="https://i.ibb.co/RCkT8Fx/ks.jpg" alt="user" />
+            <h1>Kaushal Saraswat</h1>
+          </Link>
+          <Link to="/" className="users">
+            <img src="https://i.ibb.co/RCkT8Fx/ks.jpg" alt="user" />
+            <h1>Kaushal Saraswat</h1>
+          </Link>
+          <Link to="/" className="users">
+            <img src="https://i.ibb.co/RCkT8Fx/ks.jpg" alt="user" />
+            <h1>Kaushal Saraswat</h1>
+          </Link>{' '}
+          <Link to="/" className="users">
+            <img src="https://i.ibb.co/RCkT8Fx/ks.jpg" alt="user" />
+            <h1>Kaushal Saraswat</h1>
+          </Link>{' '}
+          <Link to="/" className="users">
+            <img src="https://i.ibb.co/RCkT8Fx/ks.jpg" alt="user" />
+            <h1>Kaushal Saraswat</h1>
+          </Link>{' '}
+          <Link to="/" className="users">
+            <img src="https://i.ibb.co/RCkT8Fx/ks.jpg" alt="user" />
+            <h1>Kaushal Saraswat</h1>
+          </Link>
+        </div>
         <Link className="link" to="/pending">
           <i className="fas fa-list"></i>
           <span>Dashboard</span>
         </Link>
       </div>
-      <Users />
     </Fragment>
   );
 };
